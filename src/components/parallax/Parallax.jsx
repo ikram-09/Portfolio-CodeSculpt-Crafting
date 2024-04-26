2db5804409cd2f5ac18c2ae29c5fb146a76df749
ikram-09 <im21560282gmail.com>
2024-04-26 18:10:11 +0500

    back color

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -8,11 +8,26 @@ const Parallax = ({ type }) => {
 
   const {scrollYProgress} = useScroll({
     target:ref,
-    offset: ['start start', 'end start']
+    offset: ['start end ', 'end start']
   })
 
-  const yBg = useTransform  (scrollYProgress, [0,1], ["0%", "100%"])
+  const yBg = useTransform  (scrollYProgress, [0,1], ["0%", "200%"])
+  const planet = useTransform  (scrollYProgress, [0,1], ["0%", "20%"])
+  const star = useTransform  (scrollYProgress, [0,1], ["20%", "0%"])
 
+  const sliderVariants = {
+    initial: {
+      x: 0,
+    },
+    animate: {
+      x: "-220%",
+      transition: {
+        repeat: Infinity,
+        repeatType: "mirror",
+        duration: 100,
+      },
+    },
+  };
 
   return (
     <div
@@ -25,10 +40,12 @@ const Parallax = ({ type }) => {
             : "linear-gradient(180deg, rgb(0 0 0), rgb(49 49 49))",
       }}
     >
-      <motion.h1 style={{y: yBg}}>{type === "services" ? "what we do?" : "what we did"}</motion.h1>
+      <motion.h1 style={{y: yBg}}   variants={sliderVariants}
+        initial="initial"
+        animate="animate">{type === "services" ? "what we do?" : "what we did"}</motion.h1>
       <motion.div className="mountains"></motion.div>
-      <motion.div className="planets"></motion.div>
-      <motion.div className="stars"></motion.div>
+      <motion.div className="planets" style={{x: planet}}></motion.div>
+      <motion.div className="stars" style={{x: star}}></motion.div>
     </div>
   );
 };
